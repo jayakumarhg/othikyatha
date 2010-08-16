fac9d96da1739c129668e03857505cc800075171
jayakumarhg@gmail.com <jayakumarhg@gmail.com@411ec4cd-e6a1-33f6-e960-6df82fda8f66>
2010-08-16 19:29:07 +0000

    merge branch into trunk

--- trunk/Othikyatha/src/com/beatabout/othikyatha/Profile.java
+++ trunk/Othikyatha/src/com/beatabout/othikyatha/Profile.java
@@ -1,7 +1,7 @@
 package com.beatabout.othikyatha;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 import android.content.SharedPreferences;
 import android.location.Location;
@@ -19,9 +19,10 @@ public class Profile  {
 	private static final String SILENT_PREF = "silentPreference";
 	private static final String VIBRATE_PREF = "vibratePreference";
 	private static final String RINGTONE_PREF = "ringtonePreference";
-	private static final String NUM_LOCATIONS_PREF = "numLocPref";
-	private static final String LATITUDE_PREF = "latitudePref";
-	private static final String LONGITUDE_PREF = "longitudePref";
+    private static final String LOCATIONS_PREF = "locsPref";
+	private static final String NUM_LOCATIONS_PREF = ".num";
+	private static final String LATITUDE_PREF = ".latitude@";
+	private static final String LONGITUDE_PREF = "longitude@";
 	
 	private int profileId;
 	private SharedPreferences sharedPreferences;
@@ -109,28 +110,29 @@ public class Profile  {
 		return getName();
 	}
 	
-	Collection<Location> getLocations() {
+	List<Location> getLocations() {
 		ArrayList<Location> locations = new ArrayList<Location>();
-		int size = sharedPreferences.getInt(NUM_LOCATIONS_PREF, 0);
+		int size = sharedPreferences.getInt(LOCATIONS_PREF + NUM_LOCATIONS_PREF, 0);
 		for (int i = 0; i < size; ++i) {
 			Location location = new Location("");
-			float latitude = sharedPreferences.getFloat(LATITUDE_PREF + i, 0.0f);
-			float longitude = sharedPreferences.getFloat(LONGITUDE_PREF + i, 0.0f);
+			float latitude = sharedPreferences.getFloat(LOCATIONS_PREF + LATITUDE_PREF + i, 0.0f);
+			float longitude = sharedPreferences.getFloat(LOCATIONS_PREF + LONGITUDE_PREF + i, 0.0f);
 			location.setLatitude(latitude);
 			location.setLongitude(longitude);
+			
 			locations.add(location);
 	  }
 		return locations;
 	}
 	
-	void setLocations(Collection<Location> locations) {
+	void setLocations(List<Location> locations) {
 		SharedPreferences.Editor editor = sharedPreferences.edit();
-		editor.putInt(NUM_LOCATIONS_PREF, locations.size());
+		editor.putInt(LOCATIONS_PREF + NUM_LOCATIONS_PREF, locations.size());
 		
 		int i = 0;
 		for (Location location : locations) {
-			editor.putFloat(LATITUDE_PREF + i, (float) location.getLatitude());
-			editor.putFloat(LONGITUDE_PREF + i, (float) location.getLongitude());
+			editor.putFloat(LOCATIONS_PREF + LATITUDE_PREF + i, (float) location.getLatitude());
+			editor.putFloat(LOCATIONS_PREF + LONGITUDE_PREF + i, (float) location.getLongitude());
 			i++;
 		}
 		editor.commit();
