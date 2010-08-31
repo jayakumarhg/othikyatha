7051434fc28e33cfb4ec7adba660a4bf5a226c39
paneendra.ba@gmail.com <paneendra.ba@gmail.com@411ec4cd-e6a1-33f6-e960-6df82fda8f66>
2010-08-31 09:02:40 +0000

    Merging the two services into one

--- trunk/Othikyatha/src/com/beatabout/othikyatha/BackgroundService.java
+++ trunk/Othikyatha/src/com/beatabout/othikyatha/BackgroundService.java
@@ -6,24 +6,34 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.media.AudioManager;
 import android.os.Bundle;
 
-public class ProximityAlertService extends IntentService {
+public class BackgroundService extends IntentService {
 	public static final int PROXIMITY_EXPIRATION = -1;
 	public static final int PROXIMITY_RADIUS_IN_METERS = 42;
 
 	public static final String PROFILE_ID = "profileId";
-	public static final String PROXIMITY_ALERT_INTENT = "com.beatabout.othikyatha.PROXIMITY_ALERT";
+	public static final String BACKGROUND_SERVICE_INTENT = "com.beatabout.othikyatha.BACKGROUND_SERVICE";
 
 	public static final String REQUEST_TYPE = "requestType";
 	public static final int REQUEST_ADD = 1;
 	public static final int REQUEST_REMOVE = 2;
 	public static final int REQUEST_UPDATE = 3;
+	public static final int REQUEST_SWITCH = 4;
+	
+	public static int PROFILE_SWITCH_NOTIFY_ID = 0;
+
+	private AudioManager audioManager;
+	private NotificationManager notificationManager;
+
 
 	private LocationManager locationManager;
 	private DataManager dataManager;
@@ -31,7 +41,7 @@ public class ProximityAlertService extends IntentService {
 	private AtomicInteger requestCode;
 	private Map<String, PendingIntent> pendingIntentsMap;
 
-	public ProximityAlertService() {
+	public BackgroundService() {
 		super("Proximity Alert");
 	}
 
@@ -44,6 +54,10 @@ public class ProximityAlertService extends IntentService {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+		notificationManager =
+			  (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
 		ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
 		dataManager = new DataManager(contextWrapper);
 		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -82,7 +96,8 @@ public class ProximityAlertService extends IntentService {
 	}
 
 	private PendingIntent getProfileSwitchPendingIntent(int profileId) {
-		Intent intent = new Intent(ProfileSwitchService.PROFILE_SWITCH_INTENT);
+		Intent intent = new Intent(BackgroundService.BACKGROUND_SERVICE_INTENT);
+		intent.putExtra(REQUEST_TYPE, REQUEST_SWITCH);
 		intent.putExtra(PROFILE_ID, profileId);
 
 		PendingIntent pendingIntent = PendingIntent.getService(
@@ -108,6 +123,38 @@ public class ProximityAlertService extends IntentService {
 		} else if (request == REQUEST_UPDATE) {
 			removeAllProximityAlerts();
 			addAllProximityAlerts();
+		} else if (request == REQUEST_SWITCH) {
+			int profileId = extras.getInt(PROFILE_ID);
+			boolean entering = extras.getBoolean(
+					LocationManager.KEY_PROXIMITY_ENTERING, true);
+
+			switchProfile(profileId, entering);
+		}
+	}
+
+	private void switchProfile(int profileId, boolean entering) {
+		// If we are leaving the current proximity we need to apply default profile
+		if (!entering) {
+			profileId = dataManager.getDefaultProfileId();
+		}
+		Profile profile = dataManager.getProfile(profileId);
+		ProfileManager.applyProfile(profile, audioManager, getContentResolver());
+
+		// Notify only if we are really changing from the active profile.
+		if (profileId != dataManager.getActiveProfileId()) {
+			int icon = R.drawable.icon;
+			CharSequence tickerText = "Profile changed to " + profile.getName();
+			Notification notification = new Notification(icon, tickerText,
+					System.currentTimeMillis() + 5000);
+			notification.flags |= Notification.FLAG_AUTO_CANCEL;
+			Intent notificationIntent = new Intent(this, ProfileListActivity.class);
+			PendingIntent contentIntent = PendingIntent.getActivity(
+					getApplicationContext(), 0, notificationIntent, 0);
+			notification.setLatestEventInfo(getApplicationContext(),
+					"Profile changed", tickerText, contentIntent);
+			notificationManager.notify(PROFILE_SWITCH_NOTIFY_ID + profileId,
+					notification);
+			dataManager.setActiveProfile(profile);
 		}
 	}
 }
